557ade557e6e51b7009be228e5a2a9b33fb632cd
zaalk97 <zaalkhaz@outlook.com>
2024-12-18 20:14:23 -0600

    corrected seeds to populate data correctly

--- seeds.sql
+++ seeds.sql
@@ -1,36 +1,27 @@
-DO $$ 
-DECLARE
-
-    finance_id INT;
-    grocery_id INT;
-    jewelry_id INT;
-    supervisor_id INT;
-    cashier_id INT;
-    stocker_id INT;
-
-BEGIN
-
-    INSERT INTO department (name) VALUES ('Finance') RETURNING id INTO finance_id;
-    INSERT INTO department (name) VALUES ('Grocery') RETURNING id INTO grocery_id;
-    INSERT INTO department (name) VALUES ('Jewelry') RETURNING id INTO jewelry_id;
 
 
-    INSERT INTO role (title, salary, department) VALUES ('Supervisor', 20000.00, finance_id) RETURNING id INTO supervisor_id;
-    INSERT INTO role (title, salary, department) VALUES ('Cashier', 10000.00, grocery_id) RETURNING id INTO cashier_id;
-    INSERT INTO role (title, salary, department) VALUES ('Stocker', 11000.00, jewelry_id) RETURNING id INTO stocker_id;
+BEGIN;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Antonia','Olindo', 1, NULL),
-       ('Damokles','Liselott', 2 , 1),
-       ('Gayathri','Felicja', 3 , 1);
+INSERT INTO department (name)
+VALUES ('Finance'),
+       ('Grocery'),
+       ('Sporting Goods');
+       
+INSERT INTO role (title, salary, department)
+VALUES ('Supervisor', 20, (SELECT id FROM department WHERE name = 'Finance')),
+       ('Cashier', 200, (SELECT id FROM department WHERE name = 'Grocery')),
+       ( 'Stocker', 300, (SELECT id FROM department WHERE name = 'Sporting Goods'));
 
-    COMMIT;
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ('Antonia','Olindo', (SELECT id FROM role WHERE title = 'Supervisor'), NULL),
+       ('Damokles','Liselott', (SELECT id FROM role WHERE title = 'Cashier') ,NULL),
+       ('Gayathri','Felicja', (SELECT id FROM role WHERE title = 'Stocker') , NULL);
 
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE NOTICE 'An error occurred: %', SQLERRM;
-        ROLLBACK;
-END $$;
+
+
+
+COMMIT;
+
 
 SELECT * FROM department;
 SELECT * FROM role;
